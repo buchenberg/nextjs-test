9726e25ac460fc2fa285cb69f16fd1643c3b627f
Gregory Buchenberger <buchenberg@users.noreply.github.com>
2023-01-03 13:44:37 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,42 +20,11 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
           <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+            <h1>Yo Next.js</h1>
           </div>
         </div>
 
@@ -67,11 +36,8 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
+              Hello API <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
-              HELLO
-            </p>
           </a>
         </div>
       </main>
